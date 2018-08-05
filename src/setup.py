5ea85cf864556882ee71cb0b7f0af581c5edc8e4
Roman Parise <pariseroman@gmail.com>
2018-08-05 13:32:00 -0700

    Added Archspec

---
+++ src/setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup( name = 'SeniorDesignSimulator' ,
+       packages = [ "action" , "archspec" , "simulated" ] )
---
+++ src/simulated.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#############
+## Imports ##
+#############
+from abc import ABC
+
+#############
+## Classes ##
+#############
+class Simulated( ABC ) :
+	pass
