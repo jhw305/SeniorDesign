16dc91cbfc12ee72f19c8efb3967652c9d19877b
Chandler <chandlerditolla6@gmail.com>
2018-08-18 18:02:15 -0700

    added state transitions file and an FSM for device

--- src/FSM.py
+++ src/FSM.py
@@ -1,13 +1,10 @@
-# Time is measure in microseconds and current is measured in milliamps
-# each device should be run on its own thread so it can concurently 
-# update its power useage and state of device.
-
-
-#########################################################
-# Sending constants from page 32 fig. 32 from datasheet #
-#########################################################
-
+###################################################################################
+# TIME WILL BE IN MICROSECONDS, CURRENT WILL BE IN MILLIAMPS                      #
+# The -10000 is a placeholder due to me being unable to find the values for each  #
+# state in either the datasheet or manual.                                        #
+###################################################################################
 from simulated import Simulated
+from abc import ABC
 import simulated
 from action import Action
 
@@ -31,16 +28,31 @@ DEBUG = True
 
 
 class State():
-    def __init__(self, physical_data):
+    """
+    Creates instance of a State for the device.
+    """
+    def __init__(self, physical_data : dict):
         self.physical_data = physical_data
 
-    def getParam(self, param):
+    def getParam(self, param : str) -> 'unknown':
+        """
+        Calls a function based off of a physical
+        parameter such as 'current' and returns the
+        corrrect value for a given state.
+        """
         return self.physical_data[param](self.physical_data)
 
 
-class Device(Simulated):
-    def __init__(self, available_states, initial_state, physical_data):
-        super(Device, self).__init__()
+class Device(Simulated , ABC):
+    """
+    Creates an instance of a device dependent on the FSM,
+    physical data, and available states used to accurately
+    model the DW1000 or other like devices. 
+    """
+    def __init__(self, available_states : dict, initial_state : State, physical_data : dict):
+        Simulated.__init__(self)
+        ABC.__init__(self)
+       # super(Device, self).__init__()
         self.available_states = available_states
         self.initial_state = initial_state
         self.dev_state = initial_state
@@ -49,79 +61,76 @@ class Device(Simulated):
         self.getAvailableNextStates()
         
 
-    def setState(self, state):
+    def setState(self, state : State):
+        """
+        Sets the current state (dev_state) to the state provided.
+        This is only called through setNextState.
+        """
         self.dev_state = state
 
-    def getState(self):
+    def getState(self) -> State:
+        """
+        Returns the current state of the device. 
+        """
         return self.dev_state
 
     def getAvailableNextStates(self):
+        """
+        Gets the possible next states from the FSM
+        and assigns them to the next_states.
+        """
         self.next_states =  self.available_states[self.dev_state]
 
-    def setNextState(self, state):
+    def setNextState(self, state : State):
+        """
+        Checks if the state provided is a valid next state
+        and if so adds an action to the action queue to
+        set the state.
+        """
         for next in self.next_states:
             if state in next:
                 super().addAction(self.setState, [state])
                 super().update()
                 break
 
-    def getParam(self, param):
+    def getParam(self, param : str) -> 'unknown':
+        """
+        Gets a piece of physical data for the device such as
+        battery life. 
+        """
         return self.physical_data[param](physical_data, self.dev_state.physical_data)
-    
-        
 
-class DeviceFSM():
-    def __init__(self):
-        self.state = "IDLE"
-        self.ampmicrosecs = 0
-        self.runtime = 0
-    
-    def idleState(self):
+    def mainloop(self, simlist):
         pass
 
-    def sendState(self, messagelen):
-        self.state = "SEND"
-        self.ampmicrosecs += OSC_STARTUP_SEND_CURRENT * OSC_STARTUP_SEND_TIME
-        self.ampmicrosecs += PLL_STARTUP_SEND_CURRENT * PLL_STARTUP_SEND_TIME
-        self.ampmicrosecs += WR_TX_DATA_SEND_CURRENT * WR_TX_DATA_SEND_TIME
-        self.ampmicrosecs += TX_SHR_SEND_CURRENT * TX_SHR_SEND_TIME
-        self.ampmicrosecs += TX_PHR_SEND_CURRENT * (TX_PHR_SEND_TIME * messagelen)  
-        self.runtime += (OSC_STARTUP_SEND_TIME + PLL_STARTUP_SEND_TIME + WR_TX_DATA_SEND_TIME 
-        + (TX_PHR_SEND_TIME*messagelen) + TX_SHR_SEND_TIME
-        ) 
-        self.state = "IDLE"
+#####################################################################
+# This is dumb but i cant think of a way to do this while making it #
+# general to each device. I won't be adding comments to these       #
+# functions because they will be replaced when a better solution    #
+# is found.                                                         #
+#####################################################################
 
-    def receiveStandardState(self):
-        self.state = "RECEIVE"
-        #do the same as sendstate from figure 33 page 32 of datasheet
+def getOffCurrent(physical_data):
+    return 0
 
+def getDeepSleepCurrent(physical_data):
+    return 0.00005
 
-    def receiveSniffState(self):
-        self.state = "RECEIVE"
-        #do similar to sendstate but now take into account 
-        #the changes for sniff state i.e. per byte intermittent
-        #lowering of current usage
+def getSleepCurrent(physical_data):
+    return 0.001
 
+def getInitCurrent(physical_data):
+    return 4
 
+def getIdleCurrent(physical_data):
+    return 18
 
-    def getAmpMicrosecs(self):
-        return self.ampmicrosecs
+def getRXCurrent(physical_data):
+    return -10000
 
-    def getState(self):
-        return self.state
+def getTXCurrent(physical_data):
+    return -10000
 
-    def runFSM(self, timetorun, pingfreq, recievesniff):
-        while timetorun > self.runtime:
-            pass
-            #Have a weighted rng to choose states based off of
-            #how frequent we ping and then call the corresponding 
-            #state to update with correct ampsmilisecs and runtime
-            #for the device until we have run for the total timetorun
-            #still needs to be written obviously
-        
-
-def getCurrent(physical_data):
-    return 5
 
 def getTime(physical_data):
     return 'time'
@@ -129,15 +138,29 @@ def getTime(physical_data):
 def getBattery(dev_data, state_data):
     battlife = battlife - state_data['current'](state_data)
 
-STATE_1 = State({'current' : getCurrent, 'time' : getTime, 'state' : 1})
-STATE_2 = State({'current' : getCurrent, 'time' : getTime, 'state' : 2})
-STATE_3 = State({'current' : getCurrent, 'time' : getTime, 'state' : 3})
-STATE_4 = State({'current' : getCurrent, 'time' : getTime, 'state' : 4})
+######################################################################################
+# FSM for the DW1000, includes states, thier delays, and the current for each state. # 
+######################################################################################
+    
+OFF_STATE = State({'current' : getOffCurrent, 'time' : getTime, 'state' : 'off_state'})
+DEEPSLEEP_STATE = State({'current' : getDeepSleepCurrent, 'time' : getTime, 'state' : 'deepsleep_state'})
+SLEEP_STATE = State({'current' : getSleepCurrent, 'time' : getTime, 'state' : 'sleep_state'})
+INIT_STATE = State({'current' : getInitCurrent, 'time' : getTime, 'state' : 'init_state'})
+IDLE_STATE = State({'current' : getIdleCurrent, 'time' : getTime, 'state' : 'idle_state'})
+RX_STATE = State({'current' : getRXCurrent, 'time' : getTime, 'state' : 'rx_state'})
+TX_STATE = State({'current' : getTXCurrent, 'time' : getTime, 'state' : 'tx_state'})
 
-INITIAL_STATE = STATE_1
-
+INITIAL_STATE = OFF_STATE
 DEVICE_DATA = {'batt_life' : getBattery}
-AVAILABLE_STATES = {STATE_1 : [(STATE_2 , 4)] , STATE_2 : [(STATE_3 , 5) , (STATE_4 , 6)] , STATE_3 : [(STATE_3 , 1) , (STATE_4 , 8)] , STATE_4 : [(STATE_1 , 12)]}
+
+AVAILABLE_STATES = {OFF_STATE : [(INIT_STATE, 3000)],
+                    DEEPSLEEP_STATE : [(INIT_STATE, 3000)],
+                    SLEEP_STATE : [(INIT_STATE, 3000)],
+                    INIT_STATE : [(IDLE_STATE, 5)],
+                    IDLE_STATE : [(RX_STATE, 0) , (TX_STATE, 0)],
+                    RX_STATE : [(IDLE_STATE, -10000) , (SLEEP_STATE, -10000) , (DEEPSLEEP_STATE, -10000)],
+                    TX_STATE : [(IDLE_STATE, -10000) , (SLEEP_STATE, -10000) , (DEEPSLEEP_STATE, -10000)]} 
+                    
 
 if __name__ == "__main__":
     battlife = 1000
@@ -145,20 +168,24 @@ if __name__ == "__main__":
     
     #checking state transitions to make sure they work
     print(device.next_states)
-    device.setNextState(STATE_2)
+    print(device.getState().getParam('current'))
+    device.setNextState(INIT_STATE)
     device.getAvailableNextStates()
     print(device.next_states)
-    device.setNextState(STATE_3)
+    print(device.getState().getParam('current'))
+    device.setNextState(IDLE_STATE)
     device.getAvailableNextStates()
     print(device.next_states)
-    device.setNextState(STATE_3)
+    print(device.getState().getParam('current'))
+    device.setNextState(INIT_STATE)
     device.getAvailableNextStates()
     print(device.next_states)
-    device.setNextState(STATE_1)
+    print(device.getState().getParam('current'))
+    device.setNextState(RX_STATE)
     device.getAvailableNextStates()
     print(device.next_states)
-
-    #    
+    print(device.getState().getParam('current'))
+    #general states work correctly
             
 
 
