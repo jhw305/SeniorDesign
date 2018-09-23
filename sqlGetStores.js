9b090d142017c42994adb3b0151535f0cd40a384
jesseCampbell <jessedc95@gmail.com>
2018-09-22 17:39:40 -0700

    Delete sqlGetStores.js

--- sqlGetStores.js
+++
@@ -1,7 +0,0 @@
-var sqlQuery = require('./sqlQuery.js');
-
-function sqlGetStores(con, sqlGetStoresCallback) {
-    sqlQuery(con, 'SELECT * FROM store', sqlGetStoresCallback);
-}
-
-module.exports = sqlGetStores;	
