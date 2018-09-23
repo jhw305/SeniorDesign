2b9a97ae7226e2675ad098f0c74251f4732c6aaa
jesseCampbell <jessedc95@gmail.com>
2018-09-22 17:39:47 -0700

    Delete sqlGetItemsFromStore.js

--- sqlGetItemsFromStore.js
+++
@@ -1,7 +0,0 @@
-var sqlQuery = require('./sqlQuery.js');
-
-function sqlGetItemsFromStore(con, storeID, sqlGetItemsFromStoreCallback) {
-    sqlQuery(con, 'SELECT * FROM items WHERE storeid = ' + storeID, sqlGetItemsFromStoreCallback);
-}
-
-module.exports = sqlGetItemsFromStore;	
