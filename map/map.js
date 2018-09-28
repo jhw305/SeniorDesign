d2f33674bbff438b23fbeb53a36fd99ab879aa60
Roman Parise <pariseroman@gmail.com>
2018-09-28 01:17:37 -0700

    Background image supported. Rotations work now.

--- map/map.js
+++ map/map.js
@@ -1,3 +1,21 @@
+// TODO Some kind of layering capability so that the user, items, and the map can be in different layers?
+// TODO Hmmmm... a more general clearing capability?
+// TODO This really should be object-oriented in some way...
+
+// Background image
+var bground_img_src = "map.jpg"
+
+function set_bground( ) {
+	var canvas = document.getElementById( "themap" ) ;
+	var mycontext = canvas.getContext( "2d" ) ;
+	var bground_img = new Image( ) ;
+	bground_img.src = bground_img_src ;
+	mycontext.drawImage( bground_img , 0 , 0 ) ;
+	// From https://stackoverflow.com/questions/23104582/scaling-an-image-to-fit-on-canvas
+	mycontext.drawImage( bground_img , 0 , 0 , bground_img.width , bground_img.height , 0 , 0 , canvas.width , canvas.height ) ;
+}
+set_bground( ) ;
+
 // Constants
 var delta = 5 ;
 var default_side_length = 10 ;
@@ -10,6 +28,9 @@ var user_exists = false ;
 
 // Stored points
 var mypoints = [ ] ;
+var last_plotted_point_x = 0 ;
+var last_plotted_point_y = 0 ;
+var at_least_one_point = false ;
 
 // Available to programmers
 // Taken from https://stackoverflow.com/questions/7812514/drawing-a-dot-on-html5-canvas
@@ -50,6 +71,40 @@ function mat_mult( A , B ) {
 	return C ;
 }
 
+// TODO: Error checking - must be same size
+function mat_add( A , B ) {
+	var C = [ ] ;
+	var ab_row_cnt = 0 ;
+	var ab_col_cnt = 0 ;
+	for ( ab_row_cnt = 0 ; ab_row_cnt < A.length ; ab_row_cnt++ ) {
+		row_in_C = [ ] ;
+		for ( ab_col_cnt = 0 ; ab_col_cnt < A[ 0 ].length ; ab_col_cnt++ ) {
+			row_in_C.push( A[ ab_row_cnt ][ ab_col_cnt ] + B[ ab_row_cnt ][ ab_col_cnt ] ) ;
+		}
+		C.push( row_in_C ) ;
+	}
+	return C ;
+}
+
+function scalar_mult( c , A ) {
+	var B = [ ] ;
+	var a_row_cnt = 0 ;
+	var a_col_cnt = 0 ;
+	for ( a_row_cnt = 0 ; a_row_cnt < A.length ; a_row_cnt++ ) {
+		var B_row = [ ] ;
+		for ( a_col_cnt = 0 ; a_col_cnt < A[ 0 ].length ; a_col_cnt++ ) {
+			B_row.push( c * A[ a_row_cnt ][ a_col_cnt ] ) ;
+		}
+		B.push( B_row ) ;
+	}
+	return B ;
+}
+
+// TODO: Error checking - must be same size
+function mat_sub( A , B ) {
+	return mat_add( A , scalar_mult( -1 , B ) ) ;
+}
+
 // From degrees to radians
 function deg_to_rad( theta_in_degrees ) {
 	return ( Math.PI / 180.0 ) * theta_in_degrees ;
@@ -59,6 +114,7 @@ function deg_to_rad( theta_in_degrees ) {
 function plot_user( user_x , user_y , theta_in_degrees , color , side_length ) {
 	var canvas = document.getElementById( "themap" ) ;
 	var mycontext = canvas.getContext( "2d" ) ;
+	var center = [ [ user_x ] , [ user_y ] ] ;
 	// Calculate each corner of the triangle
 	// Get the position of each vertex before rotation
 	A = new Array( 2 ) ;
@@ -83,10 +139,12 @@ function plot_user( user_x , user_y , theta_in_degrees , color , side_length ) {
 	T[ 0 ][ 1 ] = -1 * Math.sin( deg_to_rad( theta_in_degrees ) ) ;
 	T[ 1 ][ 0 ] = Math.sin( deg_to_rad( theta_in_degrees ) ) ;
 	T[ 1 ][ 1 ] = Math.cos( deg_to_rad( theta_in_degrees ) ) ;
-	// Perform rotation
-	final_A = mat_mult( T , A ) ;
-	final_B = mat_mult( T , B ) ;
-	final_C = mat_mult( T , C ) ;
+	// 1) Move origin to center of triangle
+	// 2) Perform rotation
+	// 3) Shift origin back to original position
+	final_A = mat_add( mat_mult( T , mat_sub( A , center ) ) , center ) ;
+	final_B = mat_add( mat_mult( T , mat_sub( B , center ) ) , center ) ;
+	final_C = mat_add( mat_mult( T , mat_sub( C , center ) ) , center ) ;
 	// Draw triangle
 	mycontext.beginPath( ) ;
 	mycontext.moveTo( final_A[ 0 ][ 0 ] , final_A[ 1 ][ 0 ] ) ;
@@ -99,10 +157,38 @@ function plot_user( user_x , user_y , theta_in_degrees , color , side_length ) {
 	mycontext.stroke( ) ;
 }
 
-function plot_all_current_points( ) {
-	var pt_cnt = 0 ;
-	for ( pt_cnt = 0 ; pt_cnt < mypoints.length ; pt_cnt++ ) {
-		plot_point( mypoints[ pt_cnt ][ 0 ] , mypoints[ pt_cnt ][ 1 ] ) ;
+// Draw a line from last point plotted to the point provided
+function draw_line( x , y ) {
+	var canvas = document.getElementById( "themap" ) ;
+	var mycontext = canvas.getContext( "2d" ) ;
+	mycontext.beginPath( ) ;
+	mycontext.moveTo( last_plotted_point_x , last_plotted_point_y ) ;
+	mycontext.lineTo( x , y ) ;
+	mycontext.strokeStyle = 'blue' ;
+	mycontext.stroke( ) ;
+}
+
+// Add another item point to the map
+// push is optional arg. Determines whether to push to mypoints.
+function next_point( x , y , push ) {
+	plot_point( x , y ) ;
+	if ( typeof( push ) === 'undefined' || push == true ) {
+		mypoints.push( [ x , y ] ) ;
+	}
+	if ( at_least_one_point == false ) {
+		at_least_one_point = true ;
+	} else {
+		draw_line( x , y ) ;
+	}
+	last_plotted_point_x = x ;
+	last_plotted_point_y = y ;
+}
+
+function plot_all_points( ) {
+	at_least_one_point = false ;
+	var point_ctr = 0 ;
+	for ( point_ctr = 0 ; point_ctr < mypoints.length ; point_ctr++ ) {
+		next_point( mypoints[ point_ctr ][ 0 ] , mypoints[ point_ctr ][ 1 ] , false ) ;
 	}
 }
 
@@ -110,8 +196,7 @@ function plot_all_current_points( ) {
 function _plot( ) {
 	var x = parseInt( document.getElementById( "x_item" ).value ) ;
 	var y = parseInt( document.getElementById( "y_item" ).value ) ;
-	plot_point( x , y ) ;
-	mypoints.push( [ x , y ] ) ;
+	next_point( x , y ) ;
 }
 
 function _plot_user( ) {
@@ -125,7 +210,8 @@ function _plot_user( ) {
 		user_exists = true ;
 	}
 	// Redraw the points/map in case it got cleared when the user moved
-	plot_all_current_points( ) ;
+	set_bground( ) ;
+	plot_all_points( ) ;
 	// Plot user's triangle
 	plot_user( x , y , theta , 'red' , default_side_length ) ;
 	// Set global vars
